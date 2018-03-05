37ec8ec062405caa92921ac536e497587a39ec48
michelkaporin <michel@kaporin.com>
2018-03-05 13:49:42 +0100

    Add code for the baseline server to compare index against

---
+++ test/src/treedb/baseline/Metadata.java
@@ -0,0 +1,13 @@
+package treedb.baseline;
+
+public class Metadata {
+    public long from;
+    public long to;
+    public byte[] data;
+
+    public Metadata(long from, long to, byte[] data) {
+        this.from = from;
+        this.to = to;
+        this.data = data;
+    }
+}
---
+++ test/src/treedb/baseline/MetadataList.java
@@ -0,0 +1,34 @@
+package treedb.baseline;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+public class MetadataList {
+    private ArrayList<Metadata> metadataList;
+
+    public MetadataList() {
+        metadataList = new ArrayList<>();
+    }
+
+    public boolean add(Metadata data) {
+        return metadataList.add(data);
+    }
+
+    public List<String> getEncodedRange(long from, long to) {
+        List<String> l = new ArrayList<String>();
+
+        for (Metadata m : metadataList) {
+            if (m.from >= from && to <= m.to) {
+                l.add(Base64.getEncoder().encodeToString(m.data));
+            }
+        }
+
+        return l;
+    }
+
+    public boolean clean() {
+        this.metadataList = new ArrayList<>();
+        return true;
+    }
+}
